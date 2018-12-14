9e439d422561e835b8c92a6ea11ce16d6874ed04
ottojolanki <ojolanki@stanford.edu>
2018-12-14 14:21:55 -0800

    hack past keras thread bug

---
+++ keras_api.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, jsonify
+from keras.applications import ResNet50
+from keras.preprocessing.image import img_to_array
+from keras.applications import imagenet_utils
+from PIL import Image
+import numpy as np
+import io
+import tensorflow as tf
+
+model = ResNet50(weights="imagenet")
+# to circumvent keras-tf bug https://github.com/keras-team/keras/issues/2397
+model._make_predict_function()
+graph = tf.get_default_graph()
+
+app = Flask(__name__)
+
+
+def prepare_image(image, target):
+    # if the image mode is not RGB, convert it
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+
+    # resize the input image and preprocess it
+    image = image.resize(target)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = imagenet_utils.preprocess_input(image)
+
+    # return the processed image
+    return image
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # initialize the data dictionary that will be returned from the
+    # view
+    data = {"success": False}
+
+    # ensure an image was properly uploaded to our endpoint
+    if request.method == "POST":
+        if request.files.get("image"):
+            # read the image in PIL format
+            image = request.files["image"].read()
+            image = Image.open(io.BytesIO(image))
+
+            # preprocess the image and prepare it for classification
+            image = prepare_image(image, target=(224, 224))
+
+            # classify the input image and then initialize the list
+            # of predictions to return to the client
+            # global graph to hack around the keras bug
+            with graph.as_default():
+                preds = model.predict(image)
+            results = imagenet_utils.decode_predictions(preds)
+            data["predictions"] = []
+
+            # loop over the results and add them to the list of
+            # returned predictions
+            for (imagenetID, label, prob) in results[0]:
+                r = {"label": label, "probability": float(prob)}
+                data["predictions"].append(r)
+
+            # indicate that the request was a success
+            data["success"] = True
+
+    # return the data dictionary as a JSON response
+    return jsonify(data)
